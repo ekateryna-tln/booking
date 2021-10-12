73e9deb2ffa2f82dc4bbf3ca8e012117485475f4
Kateryna Yaroshenko <k.iaroshenko@betolimp.com>
2021-10-12 11:10:19 +0300

    added images and file server

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,5 +18,8 @@ func routes(appConfig *config.AppConfig) http.Handler {
 	mux.Get("/", hendlers.Repo.Home)
 	mux.Get("/about", hendlers.Repo.About)
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
